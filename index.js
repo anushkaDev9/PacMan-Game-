d037ff83eff876cfdae9687f9e266ddf4f5b56cb
anushka <anushkanil1@gmail.com>
2024-01-26 15:55:44 -0800

    chnages

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ var pos = 0;
 let pageWidth = window.innerWidth;
 
 var direction = 0;
-
+function get_random_color() {
+    var letters = 'ABCDE'.split('');
+    var color = '#';
+    for (var i = 0; i < 3; i++) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    console.log(color)
+    return color;
+  }
 
 function Run() {
+    let div=document.getElementById('game')
 let divWidth=document.getElementById('game').offsetWidth
 console.log(divWidth)
   let img = document.getElementById("pacMan");
@@ -12,6 +21,9 @@ console.log(divWidth)
   focus = (focus + 1) % 2;
   direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
   if (direction) {
+    color=get_random_color()
+    console.log(color)
+   div.style.backgroundColor=color
     pos=0
     direction=0
     if(direction==0){
@@ -27,7 +39,6 @@ setInterval(Run, 300);
 
 function checkPageBounds(direction, imgWidth, pos, divWidth) {
   if (direction == 0 && pos + imgWidth >(divWidth-200)) {
-    console.log(pos + imgWidth)
     direction = 1;
   }
 
@@ -35,3 +46,4 @@ function checkPageBounds(direction, imgWidth, pos, divWidth) {
 
   return direction;
 }
+
